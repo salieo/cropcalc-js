55b9ee66237119db05062eee7b9578ec2914520b
Andrew Bagshaw <abagshaw@hotmail.ca>
2017-11-24 13:45:43 -0800

    Overhaul option names and Salieo API data format.

--- src/cropcalc.js
+++ src/cropcalc.js
@@ -28,37 +28,37 @@ function setOpts(standard, user) {
 }
 
 function convertDataToPercent(data, originalWidth, originalHeight) {
-    for (var i = 0; i < data["suggested-crops"].length; i++) {
-        data["suggested-crops"][i]["x1"] /= originalWidth;
-        data["suggested-crops"][i]["x2"] /= originalWidth;
-        data["suggested-crops"][i]["y1"] /= originalHeight;
-        data["suggested-crops"][i]["y2"] /= originalHeight;
+    for (var i = 0; i < data.crops.suggested.length; i++) {
+        data.crops.suggested[i].x1 /= originalWidth;
+        data.crops.suggested[i].x2 /= originalWidth;
+        data.crops.suggested[i].y1 /= originalHeight;
+        data.crops.suggested[i].y2 /= originalHeight;
     }
-    for (var i = 0; i < data["fallback-crops"].length; i++) {
-        data["fallback-crops"][i]["x1"] /= originalWidth;
-        data["fallback-crops"][i]["x2"] /= originalWidth;
-        data["fallback-crops"][i]["y1"] /= originalHeight;
-        data["fallback-crops"][i]["y2"] /= originalHeight;
+    for (var i = 0; i < data.crops.fallback.length; i++) {
+        data.crops.fallback[i].x1 /= originalWidth;
+        data.crops.fallback[i].x2 /= originalWidth;
+        data.crops.fallback[i].y1 /= originalHeight;
+        data.crops.fallback[i].y2 /= originalHeight;
     }
 }
 
 function correctCenter(xCenter, yCenter, width, height, boundaryCrop) {
-    if (xCenter - (width / 2) < boundaryCrop["x1"]) {
-        xCenter = boundaryCrop["x1"] + (width / 2);
-    } else if (xCenter + (width / 2) > boundaryCrop["x2"]) {
-        xCenter = boundaryCrop["x2"] - (width / 2);
+    if (xCenter - (width / 2) < boundaryCrop.x1) {
+        xCenter = boundaryCrop.x1 + (width / 2);
+    } else if (xCenter + (width / 2) > boundaryCrop.x2) {
+        xCenter = boundaryCrop.x2 - (width / 2);
     }
-    if (yCenter - (height / 2) < boundaryCrop["y1"]) {
-        yCenter = boundaryCrop["y1"] + (height / 2);
-    } else if (yCenter + (height / 2) > boundaryCrop["y2"]) {
-        yCenter = boundaryCrop["y2"] - (height / 2);
+    if (yCenter - (height / 2) < boundaryCrop.y1) {
+        yCenter = boundaryCrop.y1 + (height / 2);
+    } else if (yCenter + (height / 2) > boundaryCrop.y2) {
+        yCenter = boundaryCrop.y2 - (height / 2);
     }
     return [xCenter, yCenter];
 }
 
 function getDetailsFromCenter(xCenter, yCenter, width, height, subjWidth, subjHeight, subjXShift, subjYShift, boundaryCrop) {
-    subjWidth = Math.min(subjWidth, boundaryCrop["x2"] - boundaryCrop["x1"]);
-    subjHeight = Math.min(subjHeight, boundaryCrop["y2"] - boundaryCrop["y1"]);
+    subjWidth = Math.min(subjWidth, boundaryCrop.x2 - boundaryCrop.x1);
+    subjHeight = Math.min(subjHeight, boundaryCrop.y2 - boundaryCrop.y1);
 
     //Shift to correct out of bounds
     centerCorrected = correctCenter(xCenter, yCenter, subjWidth, subjHeight, boundaryCrop);
@@ -67,10 +67,10 @@ function getDetailsFromCenter(xCenter, yCenter, width, height, subjWidth, subjHe
     yCenter = centerCorrected[1] + subjYShift;
 
     centerCorrected = correctCenter(xCenter, yCenter, width, height, {
-        "x1": 0,
-        "x2": 1,
-        "y1": 0,
-        "y2": 1
+        x1: 0,
+        x2: 1,
+        y1: 0,
+        y2: 1
     });
 
     var x1 = centerCorrected[0] - (width / 2);
@@ -79,46 +79,46 @@ function getDetailsFromCenter(xCenter, yCenter, width, height, subjWidth, subjHe
     var y2 = centerCorrected[1] + (height / 2);
 
     return {
-        "x1": x1,
-        "y1": y1,
-        "x2": x2,
-        "y2": y2
+        x1: x1,
+        y1: y1,
+        x2: x2,
+        y2: y2
     };
 }
 
 function findBestCrop(crops, fallbackCrops, targetWidthPercent, targetHeightPercent, subjWidthPercent, subjHeightPercent, subjXShift, subjYShift, options) {
     var boundaryCrop = {
-        "x1": 0,
-        "x2": 1,
-        "y1": 0,
-        "y2": 1
+        x1: 0,
+        x2: 1,
+        y1: 0,
+        y2: 1
     };
-    if (crops.length === 0 || !options["zoom"]) {
+    if (crops.length === 0 || !options.zoom) {
         crops.unshift(boundaryCrop);
     }
 
-    var lastCropWidth = (crops[crops.length - 1]["x2"] - crops[crops.length - 1]["x1"]);
-    var lastCropHeight = (crops[crops.length - 1]["y2"] - crops[crops.length - 1]["y1"]);
+    var lastCropWidth = (crops[crops.length - 1].x2 - crops[crops.length - 1].x1);
+    var lastCropHeight = (crops[crops.length - 1].y2 - crops[crops.length - 1].y1);
 
     var allCrops = crops.concat(fallbackCrops);
 
-    if (options["zoom"] !== "max") {
+    if (options.zoom !== "max") {
         var finalScaleFactor = Math.max((lastCropWidth / subjWidthPercent), (lastCropHeight / subjHeightPercent));
-        if(!options["zoom"]) {
+        if(!options.zoom) {
             finalScaleFactor = Infinity;
-        } else if (typeof options["zoom"] === "number") {
-            finalScaleFactor = (1 / options["zoom"]) / (targetWidthPercent * targetHeightPercent);
-        } else if (options["zoom"] === "focus-fit") {
-            if(options["focus-region"]["horizontal"]) {
+        } else if (typeof options.zoom === "number") {
+            finalScaleFactor = (1 / options.zoom) / (targetWidthPercent * targetHeightPercent);
+        } else if (options.zoom === "focus") {
+            if(subjWidthPercent !== targetWidthPercent) { //Check to see if a focus region has been specified for horizontal direction
                 var leftPercentage = 0.5 - (subjXShift / targetWidthPercent);
                 var rightPercentage = 1 - leftPercentage;
-                var centerX = (allCrops[allCrops.length - 1]["x1"] + allCrops[allCrops.length - 1]["x2"]) / 2;
+                var centerX = (allCrops[allCrops.length - 1].x1 + allCrops[allCrops.length - 1].x2) / 2;
                 finalScaleFactor = Math.min(finalScaleFactor, (1 - centerX) / (rightPercentage * targetWidthPercent), (centerX) / (leftPercentage * targetWidthPercent));
             }
-            if (options["focus-region"]["vertical"]) {
+            if (subjHeightPercent !== targetHeightPercent) { //Check to see if a focus region has been specified for vertical direction
                 var topPercentage = 0.5 - (subjYShift / targetHeightPercent);
                 var bottomPercentage = 1 - topPercentage;
-                var centerY = (allCrops[allCrops.length - 1]["y1"] + allCrops[allCrops.length - 1]["y2"]) / 2;
+                var centerY = (allCrops[allCrops.length - 1].y1 + allCrops[allCrops.length - 1].y2) / 2;
                 finalScaleFactor = Math.min(finalScaleFactor, (1 - centerY) / (bottomPercentage * targetHeightPercent), (centerY) / (topPercentage * targetHeightPercent));
             }
         }
@@ -135,22 +135,22 @@ function findBestCrop(crops, fallbackCrops, targetWidthPercent, targetHeightPerc
     subjWidthPercent = subjWidthPercent.toFixed(10);
     subjHeightPercent = subjHeightPercent.toFixed(10);
 
-    var centerPointX = (allCrops[0]["x1"] + allCrops[0]["x2"]) / 2;
-    var centerPointY = (allCrops[0]["y1"] + allCrops[0]["y2"]) / 2;
+    var centerPointX = (allCrops[0].x1 + allCrops[0].x2) / 2;
+    var centerPointY = (allCrops[0].y1 + allCrops[0].y2) / 2;
     var continueDown;
     for (var i = 0; i < allCrops.length - 1; i++) {
         continueDown = false;
-        if (subjWidthPercent < (allCrops[i]["x2"] - allCrops[i]["x1"])) {
+        if (subjWidthPercent < (allCrops[i].x2 - allCrops[i].x1)) {
             continueDown = true;
-            boundaryCrop["x1"] = allCrops[i]["x1"];
-            boundaryCrop["x2"] = allCrops[i]["x2"];
-            centerPointX = (allCrops[i + 1]["x1"] + allCrops[i + 1]["x2"]) / 2;
+            boundaryCrop.x1 = allCrops[i].x1;
+            boundaryCrop.x2 = allCrops[i].x2;
+            centerPointX = (allCrops[i + 1].x1 + allCrops[i + 1].x2) / 2;
         }
-        if (subjHeightPercent < (allCrops[i]["y2"] - allCrops[i]["y1"])) {
+        if (subjHeightPercent < (allCrops[i].y2 - allCrops[i].y1)) {
             continueDown = true;
-            boundaryCrop["y1"] = allCrops[i]["y1"];
-            boundaryCrop["y2"] = allCrops[i]["y2"];
-            centerPointY = (allCrops[i + 1]["y1"] + allCrops[i + 1]["y2"]) / 2;
+            boundaryCrop.y1 = allCrops[i].y1;
+            boundaryCrop.y2 = allCrops[i].y2;
+            centerPointY = (allCrops[i + 1].y1 + allCrops[i + 1].y2) / 2;
         }
 
         if (!continueDown) {
@@ -165,38 +165,33 @@ function findBestCrop(crops, fallbackCrops, targetWidthPercent, targetHeightPerc
 }
 
 function convertCropToPixels(crop, actualWidth, actualHeight) {
-    crop["x1"] = Math.round(crop["x1"] * actualWidth);
-    crop["x2"] = Math.round(crop["x2"] * actualWidth);
-    crop["y1"] = Math.round(crop["y1"] * actualHeight);
-    crop["y2"] = Math.round(crop["y2"] * actualHeight);
+    crop.x1 = Math.round(crop.x1 * actualWidth);
+    crop.x2 = Math.round(crop.x2 * actualWidth);
+    crop.y1 = Math.round(crop.y1 * actualHeight);
+    crop.y2 = Math.round(crop.y2 * actualHeight);
 }
 
 function validateInput(dataPassed, userOptions) {
-    if(!dataPassed || !userOptions) {
-        throw new Error("Missing Salieo API data and/or crop options!");
-    }
-    else if(!userOptions.hasOwnProperty("target-width") || !userOptions.hasOwnProperty("target-height")) {
-        throw new Error("Missing target-width and/or target-height in crop options!");
-    }
-    else if(!dataPassed.hasOwnProperty("suggested-crops") || !dataPassed.hasOwnProperty("fallback-crops") || !dataPassed.hasOwnProperty("original-width") || !dataPassed.hasOwnProperty("original-height")) {
-        throw new Error("Salieo API data is malformed or missing information!");
-    }
+    console.assert(userOptions && userOptions.target_width && userOptions.target_height, "Missing target width/height in options!");
+    console.assert(dataPassed && dataPassed.crops 
+        && dataPassed.crops.suggested && dataPassed.crops.fallback
+        && dataPassed.orig_width && dataPassed.orig_height, "Salieo API data is malformed/missing information.");
 }
 
 function findCrop(dataPassed, userOptions) {
     data = copy(dataPassed);
-    validateInput(data, userOptions);
+    
+    validateInput(dataPassed, userOptions); //Validating input each time may be unnessecary for certain production environments. Maybe this should be removed?
 
     var options = {
-        "zoom": true
+        zoom: false,
+        actual_width: data.orig_width,
+        actual_height: data.orig_height
     }
     setOpts(options, userOptions);
-    if (!options.hasOwnProperty("actual-width") || !options.hasOwnProperty("actual-height")) {
-        options["actual-width"] = data["original-width"];
-        options["actual-height"] = data["original-height"];
-    }
-    var targetWidthPercent = options["target-width"] / options["actual-width"];
-    var targetHeightPercent = options["target-height"] / options["actual-height"];
+
+    var targetWidthPercent = options.target_width / options.actual_width;
+    var targetHeightPercent = options.target_height / options.actual_height;
 
     //Correct targetWidthPercent and targetHeightPercent if one/both are greater than 1
     if(targetWidthPercent > 1 || targetHeightPercent > 1) {
@@ -209,36 +204,30 @@ function findCrop(dataPassed, userOptions) {
     var subjYShift = 0;
     var subjWidthPercent = targetWidthPercent;
     var subjHeightPercent = targetHeightPercent;
-    if (options.hasOwnProperty("focus-region")) {
-        if(options["focus-region"].hasOwnProperty("x1") && options["focus-region"].hasOwnProperty("x2")) {
-            var x1 = options["focus-region"]["x1"];
-            var x2 = options["focus-region"]["x2"];
-            subjXShift = (0.5 - (((x1 + x2) / 2) / options["target-width"])) * (options["target-width"] / options["actual-width"]);
-            subjWidthPercent *= (x2 - x1) / options["target-width"];
-            options["focus-region"]["horizontal"] = true;
-        } else {
-            options["focus-region"]["horizontal"] = false;
+    if (options.hasOwnProperty("focus")) {
+        if(options.focus.hasOwnProperty("x1") || options.focus.hasOwnProperty("x2")) {
+            var x1 = options.focus.x1 ? options.focus.x1 : 0;
+            var x2 = options.focus.x2 ? options.focus.x2 : options.target_width;
+            subjXShift = (0.5 - (((x1 + x2) / 2) / options.target_width)) * (options.target_width / options.actual_width);
+            subjWidthPercent *= (x2 - x1) / options.target_width;
         }
         
-        if(options["focus-region"].hasOwnProperty("y1") && options["focus-region"].hasOwnProperty("y2")) {
-            var y1 = options["focus-region"]["y1"];
-            var y2 = options["focus-region"]["y2"];
-            subjYShift = (0.5 - (((y1 + y2) / 2) / options["target-height"])) * (options["target-height"] / options["actual-height"]);
-            subjHeightPercent *= (y2 - y1) / options["target-height"];
-            options["focus-region"]["vertical"] = true;
-        } else {
-            options["focus-region"]["vertical"] = false;
+        if(options.focus.hasOwnProperty("y1") || options.focus.hasOwnProperty("y2")) {
+            var y1 = options.focus.y1 ? options.focus.y1 : 0;
+            var y2 = options.focus.y2 ? options.focus.y2 : options.target_height;
+            subjYShift = (0.5 - (((y1 + y2) / 2) / options.target_height)) * (options.target_height / options.actual_height);
+            subjHeightPercent *= (y2 - y1) / options.target_height;
         }
     }
 
-    data["suggested-crops"].sortOn("id");
-    data["fallback-crops"].sortOn("id");
-    convertDataToPercent(data, data["original-width"], data["original-height"]);
+    data.crops.suggested.sortOn("id");
+    data.crops.fallback.sortOn("id");
+    convertDataToPercent(data, data.orig_width, data.orig_height);
 
-    var finalCrop = findBestCrop(data["suggested-crops"], data["fallback-crops"], targetWidthPercent, targetHeightPercent, subjWidthPercent, subjHeightPercent, subjXShift, subjYShift, options);
+    var finalCrop = findBestCrop(data.crops.suggested, data.crops.fallback, targetWidthPercent, targetHeightPercent, subjWidthPercent, subjHeightPercent, subjXShift, subjYShift, options);
 
     //Convert from percentage values to pixels for output
-    convertCropToPixels(finalCrop, options["actual-width"], options["actual-height"]);
+    convertCropToPixels(finalCrop, options.actual_width, options.actual_height);
 
     return finalCrop;
 }
--- test/test.js
+++ test/test.js
@@ -2,28 +2,30 @@ const test = require('tape');
 const cropcalc = require('../src/cropcalc.js');
 
 const data1 = {
-    "suggested-crops": [{
-        "x2": 1998,
-        "y2": 1444,
-        "y1": 552,
-        "x1": 0,
-        "id": 1
-    }, {
-        "x2": 1998,
-        "y2": 1444,
-        "y1": 646,
-        "x1": 962,
-        "id": 2
-    }],
-    "fallback-crops": [{
-        "x2": 1694,
-        "y2": 955,
-        "y1": 679,
-        "x1": 1427,
-        "id": 1
-    }],
-    "original-width": 2000,
-    "original-height": 1446
+    "crops": {
+        "suggested": [{
+            "x2": 1998,
+            "y2": 1444,
+            "y1": 552,
+            "x1": 0,
+            "id": 1
+        }, {
+            "x2": 1998,
+            "y2": 1444,
+            "y1": 646,
+            "x1": 962,
+            "id": 2
+        }],
+        "fallback": [{
+            "x2": 1694,
+            "y2": 955,
+            "y1": 679,
+            "x1": 1427,
+            "id": 1
+        }],
+    },
+    "orig_width": 2000,
+    "orig_height": 1446
 } //Salieo API data for https://www.salieo.com/testimg/test1.jpg
 
 function testCrop(t, data, options, expected) {
@@ -41,97 +43,97 @@ function testCrop(t, data, options, expected) {
 
 test('data1 400x400 zoom-off', (t) => {
     const options = {
-        "target-width": 400,
-        "target-height": 400,
-        "zoom": false
+        target_width: 400,
+        target_height: 400,
+        zoom: false
     };
     const expected = {x1: 552, y1: 0, x2: 1998, y2: 1446};
 
     testCrop(t, data1, options, expected);
 });
 
-test('data1 400x400 zoom-on', (t) => {
+test('data1 400x400 zoom-auto', (t) => {
     const options = {
-        "target-width": 400,
-        "target-height": 400,
-        "zoom": true
+        target_width: 400,
+        target_height: 400,
+        zoom: "auto"
     };
     const expected = {x1: 962, x2: 1998, y1: 410, y2: 1446};
 
     testCrop(t, data1, options, expected);
 });
 
-test('data1 400x400 zoom-on actual-width actual-height', (t) => {
+test('data1 400x400 zoom-auto actual-width actual-height', (t) => {
     const options = {
-        "target-width": 400,
-        "target-height": 400,
-        "actual-width": 1000,
-        "actual-height": 723,
-        "zoom": true
+        target_width: 400,
+        target_height: 400,
+        actual_width: 1000,
+        actual_height: 723,
+        zoom: "auto"
     };
     const expected = {x1: 481, y1: 205, x2: 999, y2: 723};
 
     testCrop(t, data1, options, expected);
 });
 
-test('data1 400x400 zoom-focus-fit focus-horizontal', (t) => {
+test('data1 400x400 zoom-focus focus-horizontal', (t) => {
     const options = {
-        "target-width": 400,
-        "target-height": 400,
-        "focus-region": {
-            "x1": 200,
-            "x2": 300
+        target_width: 400,
+        target_height: 400,
+        focus: {
+            x1: 200,
+            x2: 300
         },
-        "zoom": "focus-fit"
+        zoom: "focus"
     };
     const expected = {x1: 828, y1: 274, x2: 2000, y2: 1446};
 
     testCrop(t, data1, options, expected);
 });
 
-test('data1 400x400 zoom-focus-fit focus-vertical', (t) => {
+test('data1 400x400 zoom-focus focus-vertical', (t) => {
     const options = {
-        "target-width": 400,
-        "target-height": 400,
-        "focus-region": {
-            "y1": 200,
-            "y2": 300
+        target_width: 400,
+        target_height: 400,
+        focus: {
+            y1: 200,
+            y2: 300
         },
-        "zoom": "focus-fit"
+        zoom: "focus"
     };
     const expected = {x1: 691, y1: 0, x2: 1998, y2: 1307};
 
     testCrop(t, data1, options, expected);
 });
 
-test('data1 400x800 zoom-on', (t) => {
+test('data1 400x800 zoom-auto', (t) => {
     const options = {
-        "target-width": 400,
-        "target-height": 800,
-        "zoom": true
+        target_width: 400,
+        target_height: 800,
+        zoom: "auto"
     };
     const expected = {x1: 1199, y1: 0, x2: 1922, y2: 1446};
 
     testCrop(t, data1, options, expected);
 });
 
-test('data1 800x400 zoom-on', (t) => {
+test('data1 800x400 zoom-auto', (t) => {
     const options = {
-        "target-width": 800,
-        "target-height": 400,
-        "zoom": true
+        target_width: 800,
+        target_height: 400,
+        zoom: "auto"
     };
     const expected = {x1: 402, y1: 646, x2: 1998, y2: 1444};
 
     testCrop(t, data1, options, expected);
 });
 
-test('data1 2000x2000 zoom-on', (t) => {
+test('data1 2000x2000 zoom-auto', (t) => {
     //Ask for too large a crop - should be scaled down to max possible size
     const options = {
-        "target-width": 2000,
-        "target-height": 2000,
-        "zoom": true
+        target_width: 2000,
+        target_height: 2000,
+        zoom: "auto"
     };
     const expected = {x1: 552, y1: 0, x2: 1998, y2: 1446};
 
